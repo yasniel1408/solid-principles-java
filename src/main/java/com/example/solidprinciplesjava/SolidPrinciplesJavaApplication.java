b3ca4d178134cdc2fe1d61088faaa1966df004e7
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-05-12 03:04:22 +0200

    first commit

---
+++ src/main/java/com/example/solidprinciplesjava/SolidPrinciplesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.solidprinciplesjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidPrinciplesJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidPrinciplesJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solidprinciplesjava/SolidPrinciplesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solidprinciplesjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidPrinciplesJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
