366d32d08017888f113a9bb58fb012959112a5e7
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-05-13 05:26:21 +0200

    refactoring

--- src/main/java/com/example/solidprinciplesjava/e_dependency_inversion_principle/fixed/Main.java
+++ src/main/java/com/example/solidprinciplesjava/e_dependency_inversion_principle/fixed/Main.java
@@ -6,7 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         IAreaCalculator iAreaCalculator = new AreaCalculator();
+        IAreaCalculator iAreaCalculator2 = new AreaCalculator2();
+
         PrintJsonAreaCalculator jsonAreaCalculator = new PrintJsonAreaCalculator(iAreaCalculator);
+        PrintJsonAreaCalculator jsonAreaCalculator2 = new PrintJsonAreaCalculator(iAreaCalculator2);
 
         Circle circle = new Circle(10);
         Square square = new Square(10);
@@ -14,7 +17,9 @@ public class Main {
         List<IShape> shapes = List.of(circle, square);
 
         String json = jsonAreaCalculator.json(shapes);
+        String json2 = jsonAreaCalculator2.json(shapes);
 
         System.out.println("sum: " + json);
+        System.out.println("sum2: " + json2);
     }
 }
